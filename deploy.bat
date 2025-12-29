cc6a16a016d5ea04822f487e646fe9d77e65cfbc
Developer <dev@local>
2025-12-29 16:48:04 +0300

    Premium PDF update

--- deploy.bat
+++ deploy.bat
@@ -2,4 +2,4 @@
 git add .
 git commit -m "Premium PDF update" 2>nul
 git push origin main
-ssh root@89.169.47.138 "cd /root/bot && git stash && git pull && git stash pop 2>/dev/null; docker compose down && docker builder prune -f && docker compose up -d --build --no-cache"
+ssh root@89.169.47.138 "cd /root/bot && git stash && git pull && git stash pop 2>/dev/null; docker compose down && docker builder prune -f && docker compose build --no-cache && docker compose up -d"
