a4a93a3e33105e9ee715304fbb25db3db0ffad56
Developer <dev@local>
2025-12-29 15:27:06 +0300

    Fix WELCOME_TEXT

--- deploy.bat
+++ deploy.bat
@@ -2,5 +2,4 @@
 git add .
 git commit -m "Fix WELCOME_TEXT"
 git push origin main
-ssh root@89.169.47.138 "cd /root/bot && git pull && docker compose down && docker compose up -d --build"
-
+ssh root@89.169.47.138 "cd /root/bot && cp .env .env.backup && git stash && git pull && cp .env.backup .env && docker compose down && docker compose up -d --build"
